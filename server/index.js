4993b28d97aaa2efbcece43bce7357e5640cb8ef
jess010 <jfreasier@gmail.com>
2017-11-02 10:55:42 -0500

    tweaks

--- server/index.js
+++ server/index.js
@@ -20,7 +20,8 @@ module.exports = app
  * keys as environment variables, so that they can still be read by the
  * Node process on process.env
  */
-if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') require('../secrets')
+if (/*process.env.NODE_ENV !== 'production' && */ process.env.NODE_ENV !== 'test') require('../secrets')
+  // Update before pushing to heroku!!!//
 
 // passport registration
 passport.serializeUser((user, done) => done(null, user.id))
